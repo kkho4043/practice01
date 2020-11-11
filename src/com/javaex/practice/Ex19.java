82c92a07ef1098d10ec9771f5e7b1ae83bc3ddb0
'kkho4043' <'kkho4043@naver.com'>
2020-11-11 18:27:43 +0900

    과제

--- src/com/javaex/practice/Ex19.java
+++ src/com/javaex/practice/Ex19.java
@@ -6,8 +6,8 @@ public class Ex19 {
 		
 		
 		long light_m_speed = light_speed * 60;
-		long light_s_speed = light_m_speed * 60;
-		long light_d_speed = light_s_speed * 24;
+		long light_h_speed = light_m_speed * 60;
+		long light_d_speed = light_h_speed * 24;
 		long light_nun_speed = light_d_speed * 365;
 
 		System.out.println("빛이 1년동안 움직이는 거리는 " + light_nun_speed +" 입니다");
